7188bb5f3946e620bdf63527171a710bfc6d6ab3
bdf2ch <bdf2ch@gmail.com>
2016-12-29 00:44:16 +0300

    temp

--- serverside/config.php
+++ serverside/config.php
@@ -9,12 +9,12 @@
 
     $services = new Services();
 
-    $db_host = "192.168.91.24";
-    //$db_host = "localhost";
-    $db_user = "wfs_base";
-    //$db_user = "root";
-    $db_password = "iuh";
-    //$db_password = "l1mpb1zk1t";
+    //$db_host = "192.168.91.24";
+    $db_host = "localhost";
+    //$db_user = "wfs_base";
+    $db_user = "root";
+    //$db_password = "iuh";
+    $db_password = "l1mpb1zk1t";
     $db_name = "wfs_base";
 
     $ldap_host = "10.50.0.1";
